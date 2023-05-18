ea7884f6810f528c9a0a4c867129db8323e6ebe9
Jawnathan <jonathan@jonathanschimpf.com>
2023-05-18 16:21:12 -0400

    update index.jsx

--- index.jsx
+++ index.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
-// test ^
+
 
 // mirage.js // backend guy stuffs 
-// import "./server"
+import "./server"
 
 
 function App() {
